7da28e765448d232164272db33cc9c4d6f435eea
solis <solis@DESKTOP-96R3VE0>
2023-04-18 21:40:56 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Bill_Console.Bill;
+
+namespace Bill_Console
+{
+    internal class Program
+    {
+            static void Main(string[] args)
+            {
+                Bill.IBillDetails bill = new billinterface(12345, "PIONEER", 1000);
+                bill.DisplayBillDetails();
+            }
+    }
+}
